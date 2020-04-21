a4e33b1a64a773a2a5b0800ec8380de1a6ca044c
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-04-21 21:43:49 +0200

    3-2 Get quote

--- 3_Front_End_Libraries/index.js
+++ 3_Front_End_Libraries/index.js
@@ -8,10 +8,19 @@ function getRandomColor() {
 }
 
 const setRandomColor = () => {
+  const quote = getQuote();
   const randomColor = getRandomColor();
   const list = document.querySelectorAll('.container-fluid, .btn');
   list.forEach((e) => {
-    e.style.transition = "all 2s";
+    e.style.transition = 'all 2s';
     e.style.backgroundColor = randomColor;
   });
+};
+
+const getQuote = async () => {
+  let result = [];
+  await fetch('https://type.fit/api/quotes')
+    .then((response) => response.json())
+    .then((data) => (result = data));
+  console.log(result);
 };
